9dba4d85706b5366410198df8e2a78d52ec2bc88
sappqa <sapp.quinn@gmail.com>
2025-07-18 10:32:39 -0400

    Added basic ipc demo

--- main.c
+++ main.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+
+#define PIPE_WRITE_FILENO 100
 
 int main(int argc, char** argv) {
     int pipe_fd[2];
-    if (pipe(&pipe_fd) == -1) {
+    if (pipe(pipe_fd) == -1) {
         exit(1);
     }
 
     pid_t pid = fork();
     if (pid == 0) {
         close(pipe_fd[0]);
-
-        execv(argv[1], NULL);
-        
-    }
-    else {
+        dup2(pipe_fd[1], PIPE_WRITE_FILENO);
         close(pipe_fd[1]);
+
+        //execv(argv[1], NULL);
+
+        for (int i = 0; i < 11; i++) {
+            char message[64];
+            snprintf(message, sizeof(message), "this is message %d\n", i);
+            write(PIPE_WRITE_FILENO, message, strlen(message));
+        }
+
+        const char* done = "leaving child process";
+        write(STDOUT_FILENO, done, strlen(done));
+        close(PIPE_WRITE_FILENO);
+    }
+    else {        
+        close(pipe_fd[1]);
+
+        char buf[512];
+        ssize_t bytes_read;
+        while ((bytes_read = read(pipe_fd[0], buf, sizeof(buf))) > 0) {
+            write(STDOUT_FILENO, buf, bytes_read);
+        }
+
+        close(pipe_fd[0]);
+
         int status;
-        waitpid(0, &status, NULL);
+        waitpid(pid, &status, 0);
     }
 
     exit(0);
