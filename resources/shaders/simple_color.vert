0f3fdc28ebe71ca5c6ad27f816c2bbc2398ddc5d
sappqa <sapp.quinn@gmail.com>
2025-08-20 16:21:43 -0400

    Added basic cube.

--- resources/shaders/simple_color.vert
+++ resources/shaders/simple_color.vert
@@ -1,6 +1,6 @@
 #version 330 core
 
-uniform mat4 uProjection = mat4(1.0);
+uniform mat4 u_proj = mat4(1.0);
  
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 color;
@@ -8,6 +8,6 @@ layout(location = 1) in vec4 color;
 out vec4 fragcolor;
 
 void main() {
-    gl_Position = uProjection * vec4(position, 1.0);
+    gl_Position = u_proj * vec4(position, 1.0);
     fragcolor = color;
 }
--- src/render_graphics.c
+++ src/render_graphics.c
@@ -1,4 +1,5 @@
 #include "render_graphics.h"
+#include "common.h"
 #include "shaders.h"
 #include "linmath.h"
 #include "glad/glad.h"
@@ -6,22 +7,52 @@
 static GLuint _simple_shaders;
 static GLuint _vao;
 static GLuint _vbo;
+static GLuint _ebo;
 
 static void _vertices_init() {
-    float vertices[] = {
-        -0.5f, -0.5, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f,
-        0.5f, -0.5, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f,
-        0.0f, 0.5, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f, 
+
+    float cube_vertices[] = {
+        0.0f, 0.0f, 0.0f,   1.0f, 0.2f, 0.2f, 1.0f,
+        1.0f, 0.0f, 0.0f,   1.0f, 0.2f, 0.2f, 1.0f,
+        1.0f, 1.0f, 0.0f,   1.0f, 0.2f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.0f,   1.0f, 0.2f, 0.2f, 1.0f,
+
+        0.0f, 0.0f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f,
+    };
+
+    int indices[] = {
+        1, 3, 0, // bottom
+        1, 2, 3,
+
+        5, 6, 7, // top
+        7, 4, 5,
+
+        0, 4, 3, // left
+        4, 7, 3,
+
+        1, 2, 6, // right
+        1, 6, 5,
+
+        3, 2, 6, // back
+        3, 6, 7,
+
+        0, 1, 5, // front
+        0, 5, 4,
     };
 
     glGenBuffers(1, &_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glGenBuffers(1, &_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glGenVertexArrays(1, &_vao);
     glBindVertexArray(_vao);
     glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
     glEnableVertexAttribArray(1);
@@ -31,9 +62,25 @@ static void _vertices_init() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
+static void _camera_init() {
+    vec3 eye = {-1.1, -7, 4};
+    vec3 target = {1, 1, 0};
+    vec3 up = {0, 0, 1};
+    mat4x4 view, perspective, proj;
+    mat4x4_look_at(view, eye, target, up);
+    mat4x4_perspective(perspective, RADIANS(45.0f), RENDER_WINDOW_ASPECT, 0.01f, 100.0f);
+    mat4x4_mul(proj, perspective, view);
+
+    glUseProgram(_simple_shaders);
+    int proj_loc = glGetUniformLocation(_simple_shaders, "u_proj");
+    glUniformMatrix4fv(proj_loc, 1, 0, (GLfloat*)proj);
+    glUseProgram(0);
+}
+
 void graphics_init() {
     _simple_shaders = shaders_init();
     _vertices_init();
+    _camera_init();
 }
 
 void render_graphics() {
@@ -42,9 +89,7 @@ void render_graphics() {
 
     glUseProgram(_simple_shaders);
     glBindVertexArray(_vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
     glUseProgram(0);
-
-
 }
